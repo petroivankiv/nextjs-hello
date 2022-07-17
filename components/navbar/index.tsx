58b1423262d90e3a2d4c9b885de51f1432bf7aeb
Petro Ivankiv <petro.ivankiv88@gmail.com>
2022-07-17 20:02:21 +0300

    style: udpated menu styles

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -9,12 +9,16 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function Example() {
+type Props = {
+  signInEnabled?: boolean;
+};
+
+export default function Navbar({ signInEnabled }: Props) {
   return (
     <Popover className="relative bg-white">
       <div className="w-full mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">
+          <div className="flex justify-start shrink-0">
             <a href="#">
               <span className="sr-only">Workflow</span>
               <img
@@ -30,7 +34,7 @@ export default function Example() {
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
           </div>
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
+          <Popover.Group as="nav" className="hidden md:flex space-x-10 mr-auto">
             {MENU_CONFIG.map((menuItem) => {
               if (!menuItem.children) {
                 return (
@@ -107,20 +111,22 @@ export default function Example() {
             })}
           </Popover.Group>
 
-          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a
-              href="#"
-              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-            >
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-              Sign up
-            </a>
-          </div>
+          {signInEnabled && (
+            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+              <a
+                href="#"
+                className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Sign in
+              </a>
+              <a
+                href="#"
+                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Sign up
+              </a>
+            </div>
+          )}
         </div>
       </div>
 
@@ -194,22 +200,28 @@ export default function Example() {
                 </nav>
               </div>
             </div>
-            <div className="py-6 px-5 space-y-6">
-              <div>
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
+
+            {signInEnabled && (
+              <div className="py-6 px-5 space-y-6">
+                <div>
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                  >
+                    Sign up
                   </a>
-                </p>
+                  <p className="mt-6 text-center text-base font-medium text-gray-500">
+                    Existing customer?{' '}
+                    <a
+                      href="#"
+                      className="text-indigo-600 hover:text-indigo-500"
+                    >
+                      Sign in
+                    </a>
+                  </p>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </Popover.Panel>
       </Transition>
